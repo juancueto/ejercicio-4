6b9da711f3f37a579aa8a67378fde675ada523b1
juancueto <juan.cueto@outlook.com>
2022-09-29 23:07:27 -0500

    Ejercicio 4

---
+++ Open-BootCampo/Ejercicio4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Open_BootCampo
+{
+    public class Ejercicio4
+    {
+        public Ejercicio4()
+        {
+        }
+
+        public string CondicionIf(int numeroIf) {
+            if (numeroIf > 0)
+            {
+                return "Es positivo";
+            }
+            else if (numeroIf == 0)
+            {
+                return "Es cero";
+            }
+            else {
+                return "Es negativo";
+            }
+        }
+
+        public void BucleWhile(int numeroWhile)
+        {
+            while (numeroWhile < 3)
+            {
+                Console.WriteLine(numeroWhile);
+                numeroWhile++;
+            }
+        }
+
+        public void BucleDoWhile(int numeroWhile)
+        {
+            do
+            {
+                Console.WriteLine(numeroWhile);
+                numeroWhile++;
+            }
+            while (numeroWhile < 3);
+        }
+
+        public void BucleFor()
+        {
+            for (int numeroFor = 0; numeroFor <= 3; numeroFor++)
+            {
+                Console.WriteLine(numeroFor);
+            }
+        }
+
+        public void CondicionSwitch(string estacion)
+        {
+            switch (estacion.ToLower())
+            {
+                case "primavera":
+                    Console.WriteLine("Estamos en primavera");
+                    break;
+                case "verano":
+                    Console.WriteLine("Estamos en verano");
+                    break;
+                case "otonho":
+                    Console.WriteLine("Estamos en otonho");
+                    break;
+                case "invierno":
+                    Console.WriteLine("Estamos en invierno");
+                    break;
+                default:
+                    Console.WriteLine("No existe la estacion");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Open-BootCampo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Open_BootCampo;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Ejercicio4 obj = new Ejercicio4();
+
+
+Console.WriteLine("===== Condicion If =====");
+Console.WriteLine(obj.CondicionIf(-5));
+
+Console.WriteLine("===== Bucle While =====");
+obj.BucleWhile(0);
+
+Console.WriteLine("===== Bucle Do While =====");
+obj.BucleDoWhile(0);
+
+Console.WriteLine("===== Bucle For =====");
+obj.BucleFor();
+
+Console.WriteLine("===== Condicion Switch =====");
+obj.CondicionSwitch("Verano");
+
+
+
